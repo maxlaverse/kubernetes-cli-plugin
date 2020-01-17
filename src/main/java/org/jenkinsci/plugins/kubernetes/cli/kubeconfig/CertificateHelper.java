f3848da6d9c6b3ca8532b1e5453c3c6f9ab765c9
Max Laverse <max@laverse.net>
2020-01-17 20:51:23 +0100

    Remove unused extractFromCertificate

--- src/main/java/org/jenkinsci/plugins/kubernetes/cli/kubeconfig/CertificateHelper.java
+++ src/main/java/org/jenkinsci/plugins/kubernetes/cli/kubeconfig/CertificateHelper.java
@@ -20,38 +20,6 @@ public abstract class CertificateHelper {
     private static final String BEGIN_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----";
     private static final String END_PRIVATE_KEY = "-----END PRIVATE KEY-----";
 
-    /**
-     * Extract the private key a client certificate from a X509 certificate and write them to disk.
-     *
-     * @param certificatCredential Jenkins certificateCredential
-     * @param clientCrtFile        path where to write of the certificate
-     * @param clientKeyFile        path where to write of the private key
-     * @throws IOException          lol
-     * @throws InterruptedException on file operation
-     */
-    public static void extractFromCertificate(StandardCertificateCredentials certificatCredential,
-                                              FilePath clientCrtFile,
-                                              FilePath clientKeyFile) throws IOException, InterruptedException {
-        try {
-            KeyStore keyStore = certificatCredential.getKeyStore();
-            String alias = keyStore.aliases().nextElement();
-            X509Certificate certificate = (X509Certificate) keyStore.getCertificate(alias);
-
-            // Get private key using passphrase
-            Key key = keyStore.getKey(alias, Secret.toString(certificatCredential.getPassword()).toCharArray());
-
-            // Write certificate
-            String encodedClientCrt = wrapCertificate(Base64.encodeBase64String(certificate.getEncoded()));
-            clientCrtFile.write(encodedClientCrt, null);
-
-            // Write private key
-            String encodedClientKey = wrapPrivateKey(Base64.encodeBase64String(key.getEncoded()));
-            clientKeyFile.write(encodedClientKey, null);
-        } catch (KeyStoreException | UnrecoverableKeyException | NoSuchAlgorithmException | CertificateEncodingException e) {
-            throw new AbortException(e.getMessage());
-        }
-    }
-
     public static String wrapPrivateKey(String encodedBody) {
         return wrapWithMarker(BEGIN_PRIVATE_KEY, END_PRIVATE_KEY, encodedBody);
     }
